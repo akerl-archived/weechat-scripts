1e071ce908583b70d9c287d98560764456bc9b17
Les Aker <me@lesaker.org>
2014-06-15 11:40:42 -0400

    attempt to make regex_highlight less crap

--- regex_highlight.rb
+++ regex_highlight.rb
@@ -1,4 +1,5 @@
 # regex_highlight.rb
+# rubocop:disable Style/LineLength, Style/GlobalVars, Style/Documentation
 
 # Copyright (c) 2014 Les Aker <me@lesaker.org>
 
@@ -23,6 +24,70 @@
 #   0.0.1 - Initial functionality
 #   0.0.2 - Cleaned up code
 
+require 'yaml'
+
+class RegexConfig
+  attr_reader :path, :rules
+
+  def initialize
+    @path = Weechat.info_get('weechat_dir', '') + '/regex_highlight.conf'
+    @rules = Hash.new { |h, k| h[k] = [] }
+    File.exist?(@path) ? reload : save
+  end
+
+  def save
+    File.open(@ath, 'w') { |fh| fh << @rules.to_yaml }
+    Weechat.print('', "Saved regex_highlight config to #{@path}")
+  end
+
+  def reload
+    @rules = File.read(@path) { |file| YAML.load file }
+    Weechat.print('', "Loaded regex_highlight config from #{@path}")
+  end
+
+  def list
+    @rules.each do |channel, ruleset|
+      Weechat.print('', "Patterns for #{channel}")
+      ruleset.each { |rule| Weechat.print('', rule) }
+    end
+  end
+
+  def add(data)
+    channel, regex = parse data
+    if @rules[channel].include? regex
+      Weechat.print('', "Regex already configured for ##{channel}: #{regex}")
+    else
+      @rules[channel] << regex
+      Weechat.print('', "Added regex for ##{channel}: #{regex}")
+    end
+  end
+
+  def remove(data)
+    channel, regex = parse data
+    if @rules[channel].include? regex
+      @rules[channel].delete(regex)
+      Weechat.print('', "Removed regex for channel ##{channel}: #{regex}")
+    else
+      Weechat.print('', "This regex isn't in the list")
+    end
+  end
+
+  def match(channel, string)
+    @rules.each do |rule_channel, ruleset|
+      next unless ['ALL', channel].include? rule_channel
+      next unless ruleset.find { |rule| string.match rule }
+      return true
+    end
+    false
+  end
+
+  private
+
+  def parse
+    Regexp.last_match.values_at 'channel', 'regex'
+  end
+end
+
 def weechat_init
   Weechat.register(
     'regex_highlight',
@@ -31,16 +96,18 @@ def weechat_init
     'MIT',
     'Provides more flexible highlighting rules',
     '',
-    '',
+    ''
   )
-  $Config_Path = Weechat.info_get('weechat_dir', '') + '/regex_highlight.conf'
   load_script
-  Weechat::WEECHAT_RC_OK
 end
 
 def load_script
-  File.exists?($Config_Path) ? load_conf : save_conf
+  $config = RegexConfig.new
+  load_hooks
+  Weechat::WEECHAT_RC_OK
+end
 
+def load_hooks
   Weechat.hook_command(
     'regex',
     'Control regex highlights',
@@ -48,92 +115,41 @@ def load_script
     'save/load dump to and load from the config file; list shows current patterns; add/del add and remove patterns',
     'save || load || list || add || del',
     'command_handler',
-    '',
+    ''
   )
   Weechat.hook_modifier('weechat_print', 'highlight_check', '')
 end
 
-def dump_rules(file, key, rules)
-  file << "\\\\#{key}\n"
-  rules.each { |rule| file << "#{rule}\n" }
-  file << "\n"
-end
-
-def save_conf
-  $rules ||= Hash.new {|h, k| h[k] = [] }
-  open($Config_Path, 'w') do |file|
-    rules = $rules.dup
-    dump_rules(file, 'ALL', rules.delete('ALL'))
-    rules.each { |channel, rules| dump_rules(file, channel, rules) }
-  end
-  Weechat.print('', "Saved regex_highlight config to #$Config_Path")
-end
-
-def load_conf
-  $rules = Hash.new {|h, k| h[k] = [] }
-  open($Config_Path) do |file|
-    key = nil
-    file.each_line do |line|
-      next if line == "\n"
-      line = line.rstrip
-      line[0,2] == '\\\\' ? key = line[2..-1] : $rules[key] << line
-    end
-  end
-  Weechat.print('', "Loaded regex_highlight config from #$Config_Path")
-end
-
-def command_handler(data, buffer, args)
+def command_handler(_, _, args)
   case args
-  when 'save'
-    save_conf
-  when 'load'
-    load_conf
-  when 'list'
-    $rules.each do |channel, ruleset|
-      Weechat.print('', "Patterns for #{channel}")
-      ruleset.each { |rule| Weechat.print('', rule) }
-    end
-  when /^add #?([\w]+) (.*)/
-    if $rules[$1].include? $2
-      Weechat.print('', 'That regex is already added')
-    else
-      Weechat.print('', "Added regex /#$2/ for channel #$1")
-      $rules[$1] << $2
-    end
-  when /^del #?([\w]+) (.*)/
-    if $rules[$1].include? $2
-      Weechat.print('', "Removed regex /#$2/ for channel #$1")
-      $rules[$1].delete($2)
-    else
-      Weechat.print('', "This regex isn't in the list")
-    end
+  when 'save' then $config.save
+  when 'load' then $config.reload
+  when 'list' then $config.list
+  when /^add #?(?<channel>[\w]+) (?<regex>.*)/ then $config.add
+  when /^del #?(?<channel>[\w]+) (?<regex>.*)/ then $config.remove
   else
     Weechat.print('', 'Syntax: [save] | [load] | [list] | [[add|del] channel pattern]')
   end
   Weechat::WEECHAT_RC_OK
 end
 
-def highlight_check(data, modifier, modifier_data, string)
-  return string unless modifier_data.match(/irc;\w+\.[#\w]+;.+/)
-
-  data = modifier_data.split(';')
+def parse_modifiers(modifiers)
+  data = modifiers.split(';')
   tags = data[2].split(',')
-  server, _, channel = data[1].partition('.')
-
-  $rules.each do |rule_channel, ruleset|
-    next unless rule_channel == 'ALL' or rule_channel == channel
-    ruleset.each do |rule|
-      next unless string.match(rule)
-
-      tags[1] = 'notify_highlight' if tags[1] == 'notify_message'
-      new_tags = tags.join(',')
-      buffer = Weechat.info_get('irc_buffer', "#{server},#{channel}")
-
-      Weechat.print_date_tags(buffer, 0, new_tags, string)
-      return ''
-    end
-  end
-
-  string
+  server, _, channel = data[1].partition('.').values_at(0, 2)
+  [server, channel, tags]
 end
 
+def highlight_check(_, _, modifier_data, string)
+  return string unless modifier_data.match(/irc;\w+\.[#\w]+;.+/)
+
+  server, channel, tags = parse_modifiers(modifier_data)
+
+  return string unless $config.match channel, string
+
+  tags[1] = 'notify_highlight' if tags[1] == 'notify_message'
+  new_tags = tags.join(',')
+  buffer = Weechat.info_get('irc_buffer', "#{server},#{channel}")
+  Weechat.print_date_tags(buffer, 0, new_tags, string)
+  ''
+end
